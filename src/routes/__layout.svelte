a675f8b542ff00e7e733c5836af377816b2667b0
Xiaoming Jia <git@meow.dev>
2022-04-09 16:43:39 -0700

    SvelteKit page loading transition.

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
+	import { fade } from 'svelte/transition';
+	import Loading from '$lib/components/Loading.svelte';
+
+	$: loading = false;
+
+	beforeNavigate(() => {
+		loading = true;
+	});
+
+	afterNavigate(() => {
+		loading = false;
+	});
+</script>
+
+<div class="wrapper">
+	{#if loading}
+		<div class="modal" in:fade out:fade>
+			<Loading />
+		</div>
+	{/if}
+	<slot />
+</div>
+
+<style>
+	.wrapper {
+		position: relative;
+	}
+	.modal {
+		display: grid;
+		place-items: center;
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: rgba(26, 18, 25, 0.63);
+		backdrop-filter: blur;
+	}
+</style>
---
+++ src/routes/about.svelte
@@ -0,0 +1,18 @@
+<script lang="ts" context="module">
+	import type { Load } from '@sveltejs/kit';
+
+	// A dummy load function that takes a while to complete
+	export const load: Load = async () => {
+		return new Promise((resolve) => {
+			const res = () =>
+				resolve({
+					status: 200
+				});
+			setTimeout(res, 1000);
+		});
+	};
+</script>
+
+<h1>About Page</h1>
+
+<a href="/">Back Home</a>
